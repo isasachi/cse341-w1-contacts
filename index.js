26083c628cd907b16293247e5c570914dfaccafb
isasachi <isaacsalirrosasc@gmail.com>
2023-11-14 23:12:33 -0500

    headers middleware

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ app.use(cors());
 // Boody parser, this makes the req body readable
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 // Main router
 app.use('/', router);
 
